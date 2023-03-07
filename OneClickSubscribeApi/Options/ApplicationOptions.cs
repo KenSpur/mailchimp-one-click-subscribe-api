1edfb09eb780c2f677ebf22cca01d741935007f2
Ken Spur <ken.spur@cegeka.com>
2023-03-07 14:35:46 +0100

    remove unused prop

--- OneClickSubscribeApi/Options/ApplicationOptions.cs
+++ OneClickSubscribeApi/Options/ApplicationOptions.cs
@@ -2,8 +2,6 @@
 
 public class ApplicationOptions
 {
-    public static string Key => nameof(ApplicationOptions);
-
     public string RedirectToForm { get; set; } = string.Empty;
     public string RedirectToSubscribed { get; set; } = string.Empty;
 }
